cf885d69d30bb2af2421d23968923f3b47eb5cf8
amrutaluriuday <116861370+amrutaluriuday@users.noreply.github.com>
2022-12-16 14:50:44 +0530

    Add files via upload

---
+++ TestNG/Activities/DemoTwo4.java
@@ -0,0 +1,9 @@
+//package suiteExample;
+import org.testng.annotations.Test;
+
+public class DemoTwo4 {
+    @Test
+    public void TestCase() {
+        System.out.println("I'm in the test case from DemoTwo Class");
+    }
+}
