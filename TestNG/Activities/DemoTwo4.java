87c97e763033a56c4e34205b9ced8174f48ac264
amrutaluriuday <116861370+amrutaluriuday@users.noreply.github.com>
2023-01-04 20:13:53 +0530

    Delete DemoTwo4.java

--- TestNG/Activities/DemoTwo4.java
+++
@@ -1,9 +0,0 @@
-//package suiteExample;
-import org.testng.annotations.Test;
-
-public class DemoTwo4 {
-    @Test
-    public void TestCase() {
-        System.out.println("I'm in the test case from DemoTwo Class");
-    }
-}
